3d488615e4b131ce949bea22724616b97a366f84
Varun Raj <varunraj0609@gmail.com>
2024-12-03 22:26:18 +0530

    Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
-import Header from "./components/Header"; // Import Header component
-import SongList from "./components/SongList"; // Import SongList component
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import SongList from "./components/SongList";
+import songsData from "./assets/songs_with_spotify_links"; // Import song data
 
 const App = () => {
-  const [filteredSongs, setFilteredSongs] = useState([]); // Store filtered songs
+  const [filteredSongs, setFilteredSongs] = useState(songsData); // Initialize with all songs
 
+  // Pass the filtered songs to the SongList component
   return (
     <div>
       <Header setFilteredSongs={setFilteredSongs} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa"; // Search icon
 import songsData from "../assets/songs_with_spotify_links"; // Import song data
+import logo from "../assets/song-logo.png";
 
 const Header = ({ setFilteredSongs }) => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -22,18 +23,19 @@ const Header = ({ setFilteredSongs }) => {
   };
 
   return (
-    <header className="bg-indigo-400 text-white py-4 fixed top-0 left-0 right-0 z-10 mx-4 my-6 rounded-lg">
+    <header className="bg-indigo-400 text-white py-4 fixed top-0 left-0 right-0 z-10 mx-4 my-6 rounded-full">
       <div className="flex justify-between items-center mx-auto px-6 max-w-screen-xl">
+        <img src={logo} alt="" className="h-12 w-36 rounded-full " />
         <h1 className="text-2xl font-bold">Song Review App</h1>
         <div className="flex items-center">
-          <div className="flex items-center bg-black text-white p-2 rounded-full">
-            <FaSearch className="text-white mr-2" />
+          <div className="flex items-center bg-indigo-200 text-white p-2 rounded-full">
+            <FaSearch className="text-black mr-2" />
             <input
               type="text"
               value={searchQuery}
               onChange={handleSearchChange} // Trigger dynamic filtering
               placeholder="Search songs..."
-              className="bg-black text-white p-2 rounded-full focus:outline-none w-56"
+              className="bg-inherit text-black p-2 rounded-full focus:outline-none w-56"
             />
           </div>
         </div>
